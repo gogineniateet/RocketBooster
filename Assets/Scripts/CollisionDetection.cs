966435c725af048e720cd2816a944143dde62c4f
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-03-19 10:22:04 +0530

    Rocket Booster game done

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -5,17 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class CollisionDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Wall")
